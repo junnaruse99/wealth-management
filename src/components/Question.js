52ebc51882596b00f07270644a03b6a9934b607a
Jun Naruse <jun.naruse@pucp.edu.pe>
2020-12-26 12:25:23 -0500

    Create Question components
    Question component is in charge of ask the user what it his/her budget. There is a validation form, number
    has to be higher than 0 and once a correct budget is inserted, the question form is going to be hidden-

---
+++ src/components/Question.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Error from './Error'
+
+const Question = ({ saveBudget, saveRemaining, updateQuestion }) => {
+
+
+    // Keep track of number
+    const [ number, saveNumber ] = useState(0);
+
+    // For authentication
+    const [ error, saveError ] = useState(false);
+
+    // Submit number
+    const addNumber = e => {
+        e.preventDefault();
+
+        // Validate
+        if( number < 1 || isNaN(number) ) {
+            saveError(true);
+            return;
+        }
+        
+        // If validation is passed
+        saveError(false);
+        saveBudget(number);
+        saveRemaining(number);
+        updateQuestion(false);
+    }
+
+    return ( 
+        <>
+            <h2>Insert budget</h2>
+
+            {error ? <Error message="The number is incorrect"/>: null }
+            <form
+                onSubmit={addNumber}
+            >
+                <div className="form-group">
+                    <input
+                        type="number"
+                        className="form-control"
+                        placeholder="Insert your budget"
+                        onChange={ e => saveNumber(parseInt(e.target.value)) }
+                    >
+                    </input>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+            </form>
+        </>
+     );
+}
+ 
+export default Question
